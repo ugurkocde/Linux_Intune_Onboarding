6ac64c27d866dd95817399012e1f1dc259714e5f
@ugurkocde <ugur.koc@protonmail.com>
2023-03-27 09:49:35 +0200

    new

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Define menu options
+# Define header and menu options
+HEADER="Linux2Intune Menu"
+MESSAGE="Select an option:"
 OPTIONS=(1 "Change Devicename"
-         2 "Install Microsoft Intune"
+         2 "Microsoft Intune - Onboarding"
          3 "Update and Upgrade System"
          4 "Display System Information"
          5 "Exit")
 
 # Show menu and get selection
-CHOICE=$(whiptail --title "Select an option" --menu "Choose an option:" 12 50 5 "${OPTIONS[@]}" 3>&1 1>&2 2>&3)
+CHOICE=$(whiptail --title "$HEADER" --msgbox "$MESSAGE" 10 50 --ok-button "OK" "${OPTIONS[@]}" 3>&1 1>&2 2>&3)
 
 # Perform action based on selection
 case $CHOICE in
